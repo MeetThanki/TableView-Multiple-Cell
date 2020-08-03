8b944d82b2fdd9cf07f5846baf45446f0b0ddc7b
MeetThanki <meetthanki39@gmail.com>
2020-08-04 00:38:04 +0530

    initial

--- test/AppDelegate.swift
+++ test/AppDelegate.swift
@@ -1,36 +1,19 @@
-//
-//  AppDelegate.swift
-//  test
-//
-//  Created by Dominic Drees on 03.08.20.
-//
-
 import UIKit
-
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
+extension UIColor {
+    var isDark: Bool {
+        var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+        self.getRed(&r, green: &g, blue: &b, alpha: &a)
+        return  (0.2126 * r + 0.7152 * g + 0.0722 * b) < 0.50
+    }
+}
--- test/ViewController.swift
+++ test/ViewController.swift
@@ -1,19 +1,46 @@
-//
-//  ViewController.swift
-//  test
-//
-//  Created by Dominic Drees on 03.08.20.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 300
     }
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int { 2 }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { 20 }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: indexPath.section == 0 ? "mainCell": "testCell", for: indexPath)
+        
+        if let cell = cell as? TestCell{
+                let randomColor = UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
+                
+                cell.bgView.backgroundColor = randomColor
+                cell.titleLabel.text = "this is a test"
+                cell.descriptionLabel.text = String(repeating: "test ", count: Int.random(in: 10...500))
+                cell.startButton.setTitle("START NOW", for: .normal)
+                
+                cell.titleLabel.textColor = randomColor.isDark ? .white : .black
+                cell.descriptionLabel.textColor = randomColor.isDark ? .white : .black
+                cell.startButton.tintColor = randomColor.isDark ? .white : .black
+        }
+        
+        
+        return cell
+    }
 
 }
 
+class TestCell: UITableViewCell {
+    @IBOutlet weak var bgView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+}
